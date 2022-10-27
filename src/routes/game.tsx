6dd8af56a07fd8e9089db7bc8f45437afd4ce6fd
Konsta Nenonen <konstanenonen@protonmail.com>
2022-10-27 14:01:04 +0300

    Made the game take a text and split it to words and display them in spawns

--- src/routes/game.tsx
+++ src/routes/game.tsx
@@ -1,7 +1,17 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './game.module.scss';
 
 function Game() {
-  return <div>This is Game</div>;
+  // eslint-disable-next-line no-unused-vars
+  const [text, setText] = useState(
+    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet ullam alias corporis molestias sequi recusandae porro officiis dicta consectetur aperiam incidunt eos iusto voluptatem commodi, nostrum aliquam eaque est iure, accusamus culpa architecto debitis minus! Commodi, atque, magni ea obcaecati ad cum distinctio exercitationem magnam modi corrupti quis. Dolorum, illo ea, dolore distinctio labore doloremque accusamus officiis obcaecati, quo vel atque fugit beatae. Est sed beatae sunt eveniet commodi eligendi, tenetur doloremque neque blanditiis at cupiditate error repellat obcaecati assumenda maxime vel corrupti accusamus nam harum quos dolores fugiat! Quia odio totam nulla, aperiam natus sapiente temporibus facilis! Sequi, vero.'
+  );
+  const wordElements = text.split(' ').map((word) => <span>{word}</span>);
+  return (
+    <div>
+      <p className={styles.wordArea}>{wordElements}</p>
+    </div>
+  );
 }
 
 export default Game;
