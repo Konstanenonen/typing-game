07601355fe3c6e724d054843c571e5f0a00a0000
Konsta Nenonen <konstanenonen@protonmail.com>
2022-10-30 09:27:27 +0200

    Now the typing text is always a summary of a random Wikipedia article

--- src/routes/game.tsx
+++ src/routes/game.tsx
@@ -1,12 +1,10 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ActiveWord from '../components/ActiveWord';
 import styles from './game.module.scss';
 
 function Game() {
   // eslint-disable-next-line no-unused-vars
-  const [text, setText] = useState(
-    'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eveniet ullam alias corporis molestias sequi recusandae porro officiis dicta consectetur aperiam incidunt eos iusto voluptatem commodi, nostrum aliquam eaque est iure, accusamus culpa architecto debitis minus! Commodi, atque, magni ea obcaecati ad cum distinctio exercitationem magnam modi corrupti quis. Dolorum, illo ea, dolore distinctio labore doloremque accusamus officiis obcaecati, quo vel atque fugit beatae. Est sed beatae sunt eveniet commodi eligendi, tenetur doloremque neque blanditiis at cupiditate error repellat obcaecati assumenda maxime vel corrupti accusamus nam harum quos dolores fugiat! Quia odio totam nulla, aperiam natus sapiente temporibus facilis! Sequi, vero.'
-  );
+  const [text, setText] = useState('');
   const [input, setInput] = useState('');
   const wordArray = text.split(' ');
   const inputArray = input.split(' ');
@@ -30,6 +28,14 @@ function Game() {
     // The word wasn't any of the above so it was spelled incorrectly
     return <span className={styles.wrong}>{word}</span>;
   });
+
+  useEffect(() => {
+    fetch('https://en.wikipedia.org/api/rest_v1/page/random/summary')
+      .then((response) => response.json())
+      .then((data) => {
+        setText(data.extract);
+      });
+  }, []);
 
   return (
     <div className={styles.container}>
