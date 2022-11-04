98d20398be79f160a28b6e1e6029d3c7e4abfa3a
konsta nenonen <konsta.nenonen@efima.com>
2022-11-04 10:12:02 +0200

    Added some type packages

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import WordArea from '../components/WordArea';
 import styles from './home.module.scss';
 
 function Home() {
   return (
     <div className={styles.container}>
       <h1>Typing game</h1>
+      <WordArea
+        loaded
+        wordElements={[<span>Typing</span>, <span>game</span>]}
+      />
       <p>Welcome to the Typing game website!</p>
       <Link to="/game">Go to game</Link>
     </div>
