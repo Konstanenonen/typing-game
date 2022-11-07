5a14a2d2d81649dda4852cfebe95a21f909a23b1
Konsta Nenonen <konstanenonen@protonmail.com>
2022-11-07 23:12:49 +0200

    Improved credits page's styling and layout

--- src/routes/credits.tsx
+++ src/routes/credits.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
+import styles from './credits.module.scss';
 
 function Credits() {
   return (
-    <div>
-      <h1>Big thank you to Wikimedia REST API</h1>
+    <div className={styles.container}>
+      <h1>
+        Big thank you to Wikimedia{' '}
+        <a href="https://www.mediawiki.org/wiki/Wikimedia_REST_API">REST API</a>
+      </h1>
+
       <p>
         Wikimedia REST API is used in this app to fetch random Wikipedia article
         summaries!
       </p>
-      <a href="https://www.mediawiki.org/wiki/Wikimedia_REST_API">
-        Link to api documentation
-      </a>
       <p>
-        Wikimedia REST API, and there for this project too, is licensed under
+        Wikimedia REST API, and therefore this project too, is licensed under{' '}
         <a href="https://creativecommons.org/licenses/by-sa/3.0/">
           CC-BY-SA 3.0
-        </a>
-        and
-        <a href="GFDL">GFDL</a>
-        licenses.
+        </a>{' '}
+        and <a href="GFDL">GFDL</a> licenses.
       </p>
     </div>
   );
