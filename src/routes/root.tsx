13e855ce71aeecd4e95d2befa7e4141dcf9d69a4
Konsta Nenonen <konstanenonen@protonmail.com>
2022-11-08 23:41:47 +0200

    Improved footer styling and content

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Outlet, NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 // eslint-disable-next-line import/no-unresolved
-import { faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar';
 import styles from './root.module.scss';
@@ -42,9 +42,22 @@ function Root() {
       </div>
       <Footer>
         <p>Made by Konsta Nenonen</p>
-        <a href="https://github.com/Konstanenonen">
-          <FontAwesomeIcon icon={faGithub} />
-        </a>
+        <div className={styles.iconArea}>
+          <a
+            href="https://github.com/Konstanenonen/typing-game"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a
+            href="https://fi.linkedin.com/in/konsta-nenonen-596767203"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+        </div>
       </Footer>
     </div>
   );
